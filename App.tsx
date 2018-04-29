a2dc722a916ebdc2a82bbac1a6ec286f84956bca
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-04-29 11:34:38 +0100

    Playing with styles

--- App.tsx
+++ App.tsx
@@ -1,83 +1,65 @@
-import * as React from 'react';
+import React from 'react';
 import {
-  StyleSheet,
+  View,
   Text,
+  Button,
+  StyleSheet,
 } from 'react-native';
 import {
-  Store,
-} from './System';
-import {
-  Provider,
-  connect,
-} from 'react-redux';
-import {
-  combineReducers,
-} from 'redux';
-import {
-  createReduxBoundAddListener,
-} from 'react-navigation-redux-helpers';
-import Routes from './routes';
-import {
-  StackNavigator,
-  addNavigationHelpers,
+  DrawerNavigator,
 } from 'react-navigation';
-
 import {
-  Navigation,
-} from './Modules';
+  example,
+} from './Components';
 
-import AppReducers from './reducers';
-import sagas from './sagas';
+class Home extends React.Component {
+  static navigationOptions: {
+    drawerLabel: 'home',
+  };
 
-const AppNavigator = StackNavigator(Routes);
-
-const navigationReducer = Navigation.reducer(AppNavigator);
-
-console.log('navigation', navigationReducer);
-
-const store = Store(combineReducers({
-  ...AppReducers,
-  navigationReducer,
-}), {}, sagas);
-
-console.log('store', store);
-
-const addListener = createReduxBoundAddListener("root");
-
-const App = () => (
-  <AppNavigator navigation={addNavigationHelpers({
-    dispatch: this.props.dispatch,
-    state: this.props.nav,
-    addListener,
-  })} />
-);
-
-const mapStateToProps = (state) => ({
-  nav: state.nav
-});
-
-const AppWithNavigationState = connect(mapStateToProps)(App);
+  render = () => (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#cccccc',
-    alignItems: 'stretch',
-    justifyContent: 'flex-start',
     flexDirection: 'column',
   },
-  scroll: {
-    flex: 10,
+  main: {
+    flex: 12,
+  },
+  header: {
+    flex: 1,
+    backgroundColor: '#FF0000',
+    paddingTop: 30,
+  },
+  icon: {
+    width: 24,
+    height: 24,
   },
 });
 
-export default class Root extends React.Component<{}> {
-  render() {
-    return (
-      <Provider store={store}>
-        <Text>Hi</Text>
-        <AppWithNavigationState />
-      </Provider>
-    );
-  }
-}
+const App = DrawerNavigator({
+  Home: {
+    screen: Home,
+  },
+  example: {
+    screen: example,
+  },
+  //initialRouteName: 'Home',
+});
+
+export default () => (
+  <View style={styles.container}>
+    <View style={styles.header}>
+      <Text>Header</Text>
+    </View>
+    <View style={styles.main}>
+      <App/>
+    </View>
+  </View>
+);
